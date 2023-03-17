c8b87c3de1b53ee9e22b8bb9f74f520b0e140652
Omri Kalman <omrkalman@gmail.com>
2023-03-17 16:05:58 +0200

    Restored jsons to smaller (pre-production)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskPageComponent } from './task-page/task-page.component';
 
